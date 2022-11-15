3975964dd37f3ee75e78e7c98cb5073f3f57e58b
2dam3 <ikacu@plaiaundi.net>
2022-11-15 12:47:50 +0100

    busqueda a medias

--- app/src/main/java/com/example/sinemaproject/SearchActivity.java
+++ app/src/main/java/com/example/sinemaproject/SearchActivity.java
@@ -1,14 +1,58 @@
 package com.example.sinemaproject;
 
+import static com.example.sinemaproject.PreLoadActivity.AllDataList;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SearchView;
 
-public class SearchActivity extends AppCompatActivity {
+import com.example.sinemaproject.adapter.ItemRecyclerAdapter;
+import com.example.sinemaproject.adapter.SearchRecyclerAdapter;
+import com.example.sinemaproject.model.AllData;
+import com.example.sinemaproject.model.CategoryItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    List<CategoryItem> search_movies;
+    private SearchView svSearch;
+    private RecyclerView rvLista;
+    SearchRecyclerAdapter searchRecyclerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        svSearch = findViewById(R.id.svSearch);
+        rvLista = findViewById(R.id.rvLista);
+
+        search_movies = new ArrayList<>();
+        for (AllData item:AllDataList) {
+            search_movies.add(new CategoryItem(item.getId(), item.getName(), item.getImageOriginal(), item.getLanguage(), item.getStatus(), item.getPremiered(), item.getEnded(), item.getSummary(), item.getGenres()));
+        }
+        searchRecyclerAdapter = new SearchRecyclerAdapter(search_movies, this);
+        RecyclerView recyclerView = findViewById(R.id.rvLista);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(searchRecyclerAdapter);
+
+        svSearch.setOnQueryTextListener(this);
+
+    }
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        searchRecyclerAdapter.filter(s);
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/sinemaproject/adapter/SearchRecyclerAdapter.java
@@ -0,0 +1,115 @@
+package com.example.sinemaproject.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.sinemaproject.MovieDetails;
+import com.example.sinemaproject.R;
+import com.example.sinemaproject.model.CategoryItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class SearchRecyclerAdapter extends RecyclerView.Adapter<SearchRecyclerAdapter.ViewHolder> {
+    private List<CategoryItem> searchList;
+    private List<CategoryItem> original_searchList;
+    private LayoutInflater minflater;
+    private Context context;
+
+    public SearchRecyclerAdapter(List<CategoryItem> searchList, Context context) {
+        this.minflater = LayoutInflater.from(context);
+        this.searchList = searchList;
+        this.context = context;
+        this.original_searchList= new ArrayList<>();
+        original_searchList.addAll(searchList);
+    }
+
+
+    @NonNull
+    @Override
+    public SearchRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = minflater.inflate(R.layout.search_item, null);
+        return new SearchRecyclerAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchRecyclerAdapter.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        Glide.with(context).load(searchList.get(position).getImageUrl()).into(holder.image);
+        holder.title.setText(searchList.get(position).getMovieName());
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, MovieDetails.class);
+                i.putExtra("movieId", searchList.get(position).getId());
+                i.putExtra("movieName", searchList.get(position).getMovieName());
+                i.putExtra("movieImageUrl",searchList.get(position).getImageUrl());
+                i.putExtra("movieLanguage", searchList.get(position).getLanguage());
+                i.putExtra("movieStatus", searchList.get(position).getStatus());
+                i.putExtra("moviePremiered", searchList.get(position).getPremiered());
+                i.putExtra("movieEnded", searchList.get(position).getEnded());
+                i.putExtra("movieSummary", searchList.get(position).getSummary());
+                i.putExtra("movieGenres", searchList.get(position).getGenres());
+                context.startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return searchList.size();
+    }
+
+    public void filter(String strSearch){
+        if(strSearch.length()== 0){
+            searchList.clear();
+            searchList.addAll(original_searchList);
+        }
+        else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                List<CategoryItem> collect = searchList.stream()
+                        .filter(item -> item.getMovieName().toLowerCase().contains(strSearch))
+                        .collect(Collectors.toList());
+                searchList.clear();
+                searchList.addAll(collect);
+            }
+            else{
+                searchList.clear();
+                for (CategoryItem item:searchList) {
+                    if(item.getMovieName().toLowerCase().contains(strSearch)){
+                        searchList.add(item);
+                    }
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    public void  setItem(List<CategoryItem> items){searchList = items;}
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView title;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.search_Image);
+            title = itemView.findViewById(R.id.search_title);
+        }
+
+    }
+}
